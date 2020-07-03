bb042c76d4cbd0cd7c118c531efe05d343f3ddd8
Jason Schwier <jasonschwier22@yahoo.com>
2020-07-03 03:03:49 -0700

    finished logo

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns = [
 
 	path('', views.index, name="index"),
+	path('about.html', views.about, name="about"),
+	path('contact.html', views.contact, name="contact"),
+	path('main.html', views.main, name="main"),
+	path('news.html', views.news, name="news"),
+	path('search.html', views.search, name="search"),
+	path('listing1.html', views.listing1, name="listing1"),
 
 
 	
--- website/views.py
+++ website/views.py
@@ -4,5 +4,23 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'index.html', {})
 
+def about(request):
+	return render(request, 'about.html', {})
+
+def contact(request):
+	return render(request, 'contact.html', {})
+
+def listing1(request):
+	return render(request, 'listing1.html', {})
+
+def main(request):
+	return render(request, 'main.html', {})
+
+def news(request):
+	return render(request, 'news.html', {})
+
+def search(request):
+	return render(request, 'search.html', {})
+
 
 
